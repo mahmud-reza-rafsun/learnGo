745fc65568298ca575d398b59fe77c250c3dca16
Mahmud Reza Rafsun <97410422+mahmud-reza-rafsun@users.noreply.github.com>
2026-06-17 13:19:02 +0600

    learn group variable

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var name string
+	// name = "Rafsun"
+
+	// name := "Rafsun"
+
+	var (
+		name string = "Rafsun"
+		age  int    = 20
+	)
+
+	// multipal variable declaration
+
+	fmt.Println(name)
+	fmt.Println(age)
+}
